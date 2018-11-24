27ae08604b29014230313b62ceb963a47e35e96a
Carol Greene <carolgreene3@gmail.com>
2018-11-24 17:02:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getRepositories() {
 
 function showRepositories() {
   var repos = JSON.parse(this.responseText);
-  console.log(repos);
+  //console.log(repos);
   const repoList = `<ul>${repos
     .map(
       r =>
